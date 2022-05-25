60c54d8c8e5d0ebdae474580c13015989cfb56ea
GonzaloNHC <101850889+GonzaloNHC@users.noreply.github.com>
2022-05-25 10:59:52 -0400

    Update Menu.java

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Menu {
+
+    public void menu(){
+        int opcion=-1;
+        while(opcion!=3){
+
+            String title="\t\t\tMENU PRINCIPAL\n\n\n";
+            System.out.println(title);
+
+            String descripcionOpciones="1-Ver estado de empresa"+
+                    "\n"+"2-editar empresa"+
+                    "\n"+"3-salir";
+
+            System.out.println(descripcionOpciones);
+
+            Scanner teclado = new Scanner(System.in);
+            opcion = teclado.nextInt();
+
+        }
+    }
 
 }
